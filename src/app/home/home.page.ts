ca7b29cecfefb528d2a9d3337a1c9fa92370e18c
Sergio Obalski Filho <serginhobalski@gmail.com>
2021-09-26 16:21:10 -0300

    App Alcool ou Gasolina

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public resultado: String = "Resultado"
+
+  calcular(){
+    //Validar se os campos foram realmente preenchidos
+    if( this.precoAlcool && this.precoGasolina ){
+      
+        var pAlcool = parseFloat(this.precoAlcool)
+        var pGasolina = parseFloat(this.precoAgasolina)
+        //Cálculo
+        var res = pAlcool / pGasolina
+        if( res >= 0.7){
+          this.resultado = "Melhor utilizar Gasolina!"
+        }else{
+          this.resultado = "Melhor utilizar Álcool!"
+        }
+    }else{
+        this.resultado = "Favor preencher os campos corretamente."
+    }
+  }
 
 }
